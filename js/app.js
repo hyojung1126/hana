f1b9bb23cede32e5799c4e9c548d88d550dc8da4
hyojung1126 <chlgywjd50@naver.com>
2023-08-01 17:26:13 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(function () {
+    // load menu
+    $('.header_page').load('../inc/header_page.html')
+    $('.footer_page').load('../inc/footer_page.html')
+
+    // slick slider
+    // $('.slide').slick({
+    //     arrows: false,
+    //     infinite: true,
+    //     speed: 300,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //     cssEase: 'linear'
+    // });
+
+    // $('.text_slide').slick({
+    //     arrows: false,
+    //     infinite: true,
+    //     speed: 700,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //     cssEase: 'linear',
+    //     slidesToShow: 1
+    // });
+
+})
+
+
+
