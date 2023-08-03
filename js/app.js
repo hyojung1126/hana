d13397bf299204ec39c55ee35e53c0ad5414bb50
hyojung1126 <chlgywjd50@naver.com>
2023-08-03 10:42:52 +0900

    hana

--- js/app.js
+++ js/app.js
@@ -23,6 +23,41 @@ $(function () {
     //     slidesToShow: 1
     // });
 
+
+// Initialize Swiper
+    var swiper = new Swiper(".mySwiper", {
+        loop:true,
+        loopAdditionalSlides: 1,
+		loopedSlides: 1,
+        autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+        pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });
+
+// switer play_stop btns
+    $('.xi-play').on('click',function(){
+        swiper.autoplay.start();
+        return false;
+    })
+
+    $('.xi-pause').on('click',function(){
+        swiper.autoplay.stop();
+        return false;
+    })
+
+    $('.play_btns i').click(function(){
+        $('.xi-pause, .xi-play').toggleClass('active')
+    })
+
 })
 
 
